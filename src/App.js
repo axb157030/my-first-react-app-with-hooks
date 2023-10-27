bcc3f908f446371616bb4ee27a8c4d790a928856
unknown <amine.benelbar.1@gmail.com>
2023-10-26 22:28:28 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [theLogo, setImage] = useState(logo);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={theLogo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <a onClick={() => setImage("https://images.photocase.com/p/pppg7bql/tro9hhuy/photocasetro9hhuy3.jpg?1679419423")}
         >
-          Learn React
+          Click me to change image to something else
         </a>
       </header>
+
+      
     </div>
   );
 }
